4ca45f73ce87d0dc5424a5d929ec648a6361f10d
Jacob Zirbel <jacobzirbel@gmail.com>
2020-04-06 20:16:22 -0500

    add database
    Co-authored-by: kayla <kaylagrogan23@gmail.com>
    Co-authored-by: Rolds18 <rolds18@yahoo.com>

--- app.js
+++ app.js
@@ -1,9 +1,37 @@
+var config = {
+	apiKey: "AIzaSyAxrrDGwvJPDmTwqNk9cAgD0L1Z8BtuHEE",
+	authDomain: "bootcamp-4087e.firebaseapp.com",
+	databaseURL: "https://bootcamp-4087e.firebaseio.com",
+	projectId: "bootcamp-4087e",
+	storageBucket: "bootcamp-4087e.appspot.com",
+};
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
 $("#submit-button").on("click", function (event) {
 	event.preventDefault();
 	var name = $("#employee-name").val();
 	var role = $("#role").val();
 	var startDate = $("#start-date").val();
 	var monthlyRate = $("#monthly-rate").val();
+
+	database.ref().push({
+		name: name,
+		role: role,
+		startDate: startDate,
+		monthlyRate: monthlyRate,
+	});
+});
+
+database.ref().on("child_added", function (snapshot) {
+	var name = snapshot.val().name;
+	var role = snapshot.val().role;
+	var startDate = snapshot.val().startDate;
+	var monthlyRate = snapshot.val().monthlyRate;
+
+	var monthsWorked = getMonthsWorked(startDate);
+	var totalBilled = monthsWorked * monthlyRate;
 
 	var newRow = $("<tr>");
 	newRow.append("<td>" + "#" + "</td>");
@@ -13,3 +41,9 @@ $("#submit-button").on("click", function (event) {
 	newRow.append("<td>" + monthlyRate + "</td>");
 	$("#table-body").append(newRow);
 });
+
+function getMonthsWorked(startDate) {
+	console.log(startDate);
+	var today = new Date();
+	console.log(today - startDate);
+}
